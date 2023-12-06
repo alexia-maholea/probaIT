477cc2ea7b9325861b1dec979a56843b888e6cc5
Alexia Maholea <alexia.maholea@gmail.com>
2023-12-06 23:54:28 +0200

    ultimele modif

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -21,8 +21,15 @@ db.on('error', (error: Error) => {
   });  
 
 const userSchema = new mongoose.Schema({
-  username: String,
-  password: String,
+    username: {
+        type: String,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true
+    }
+
 });
 
 const User = mongoose.model('User', userSchema);
